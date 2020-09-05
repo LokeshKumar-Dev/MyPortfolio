96359849582dd9b238347e1a8ee829043a91f7b5
lokesh <adobep40@gmail.com>
2020-09-04 22:36:56 -0700

    Updates

---
+++ precache-manifest.93cf304adcc7fe5161bff920c7238120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a153f2ffeb69412b469287afa924fb7",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "45ac4436936d653decfa",
+    "url": "/MyPortfolio/static/css/main.a8115f6c.chunk.css"
+  },
+  {
+    "revision": "82aae371f81e02685e50",
+    "url": "/MyPortfolio/static/js/2.79cfd48f.chunk.js"
+  },
+  {
+    "revision": "3ccf7c4e9a877af4932536beea058488",
+    "url": "/MyPortfolio/static/js/2.79cfd48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ac4436936d653decfa",
+    "url": "/MyPortfolio/static/js/main.86ee576b.chunk.js"
+  },
+  {
+    "revision": "0d8bee11d1469486e213",
+    "url": "/MyPortfolio/static/js/runtime-main.7e73d9a0.js"
+  }
+]);
