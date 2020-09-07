9635490f9fc7b0081c441e890cf715737aa20910
lokesh <adobep40@gmail.com>
2020-09-07 06:36:08 -0700

    Updates

---
+++ precache-manifest.da9eb4b64600100f2c6d49c24360abe0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d950351766bad002fa92cf79d136dd50",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6edd151fe1a758e6d9bf",
+    "url": "./static/css/main.a8115f6c.chunk.css"
+  },
+  {
+    "revision": "9519644e53849fcfb38e",
+    "url": "./static/js/2.574fa4d3.chunk.js"
+  },
+  {
+    "revision": "258d524c1604b7b86ee331f91d5ba31c",
+    "url": "./static/js/2.574fa4d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6edd151fe1a758e6d9bf",
+    "url": "./static/js/main.71b295ce.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "6978953e2528db740505b60e3e70a4f1",
+    "url": "./static/media/about-phone.6978953e.webp"
+  },
+  {
+    "revision": "a2c4e8a5630d8ab8c4813bc093cafed0",
+    "url": "./static/media/about.a2c4e8a5.jpg"
+  },
+  {
+    "revision": "695144bfc533621ed67498759b3c5e29",
+    "url": "./static/media/estore.695144bf.jpg"
+  },
+  {
+    "revision": "e83096eb80689d8bce0582ebe3bb1d3d",
+    "url": "./static/media/hero-tab_port.e83096eb.png"
+  },
+  {
+    "revision": "6a16548a402aa78541102368e9825a00",
+    "url": "./static/media/hero.6a16548a.png"
+  },
+  {
+    "revision": "f411ed000fb7207f6d2925678e7f62a0",
+    "url": "./static/media/movie.f411ed00.jpg"
+  },
+  {
+    "revision": "5d8bab56e530be9c2cddb184c119d342",
+    "url": "./static/media/weather.5d8bab56.jpg"
+  }
+]);
